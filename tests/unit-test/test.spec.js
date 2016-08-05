34a6db5e338c45c79eb08fd17d37c638088ab89e
blackdante <kamil.m.kielbasa@gmail.com>
2016-08-05 22:24:20 +0200

    move test with supertest to integration folder

--- tests/unit-test/test.spec.js
+++ tests/unit-test/test.spec.js
@@ -1,22 +1,7 @@
-/*eslint global-require: 0*/
+const assert = require('chai').assert;
 
-const request = require('supertest');
-const STATUS_OK = 200;
-
-describe('loading express', () => {
-  let server;
-
-  beforeEach(() => {
-    server = require('../../bin/www');
-  });
-
-  afterEach(() => {
-    server.close();
-  });
-
-  it('responds to /', (done) => {
-    request(server)
-      .get('/')
-      .expect(STATUS_OK, done);
+describe('test', () => {
+  it('should pass', () => {
+    assert.equal(1, 1);
   });
 });
